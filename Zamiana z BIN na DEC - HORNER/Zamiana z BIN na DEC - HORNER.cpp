decf2d4a1030e64bd53d1d1ece672acf3974358e
HirsuiteLlama276 <60354361+HirsuiteLlama276@users.noreply.github.com>
2021-06-10 12:07:02 +0200

    Add files via upload

---
+++ Zamiana z BIN na DEC - HORNER/Zamiana z BIN na DEC - HORNER.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+long oblicz(int A[], int n)
+{
+    long w = A[0];
+    for (int i = 1; i <= n; i++) w = w * 2 + A[i];
+    return w;
+}
+int main()
+{
+    int tab[8] = {1,1,1,1,1,1,1,1};
+    int n = 7;
+
+    cout << oblicz(tab, n);
+}
---
+++ Zamiana z HEX na DEC - HORNER/Zamiana z HEX na DEC - HORNER.cpp
@@ -0,0 +1,44 @@
+﻿// Zamiana z HEX na DEC - HORNER.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+using namespace std;
+
+long oblicz(string hex)
+{
+    double w =0;
+    int pot = 0;
+    for (int i = hex.size() - 1; i >= 0; i--)
+    {
+        if ((int)hex[i] - 48 < 10)
+        {
+            w = w + (pow(16, pot) * ((int)hex[i] - 48));
+        }
+        else w = w + (pow(16, pot) * ((int)hex[i] - 55));
+        pot++;
+    }
+    return w;
+}
+
+
+int main()
+{
+    string hex;
+    cout << "Wpisz liczbe szesnastkowa: ";
+    cin >> hex;
+    cout << endl;
+    cout << "Twoja liczba w systemie 16: " << hex << endl;
+    cout << "Twoja liczba w systemie 10: " << oblicz(hex);
+}
+
+
+// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
+// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
+
+// Porady dotyczące rozpoczynania pracy:
+//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
+//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
+//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
+//   4. Użyj okna Lista błędów, aby zobaczyć błędy
+//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
+//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
